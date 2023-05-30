f38a24b8673fc232ef887a0b10c10dbc7544709c
Marek Rogalski <mafikpl@gmail.com>
2023-05-30 15:52:52 +0200

    Use local base64 implementation instead of OpenSSL

--- base64.cc
+++ base64.cc
@@ -1,18 +1,35 @@
 #include "base64.hh"
 
-#include <openssl/bio.h>
-#include <openssl/evp.h>
+static const unsigned char kBase64Chars[65] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-std::string Base64Encode(std::string_view str) {
-  BIO *b64 = BIO_new(BIO_f_base64());
-  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
-  BIO *mem = BIO_new(BIO_s_mem());
-  BIO_push(b64, mem);
-  BIO_write(b64, str.data(), str.size());
-  BIO_flush(b64);
-  char *ptr;
-  long size = BIO_get_mem_data(mem, &ptr);
-  std::string ret(ptr, size);
-  BIO_free_all(b64);
-  return ret;
+std::string Base64Encode(std::string_view in) {
+  const int out_size = (in.size() + 2) / 3 * 4;
+  char out_buf[out_size];
+  char *p = out_buf;
+  const uint8_t *src = (const uint8_t *)in.data();
+  int len = in.size();
+  while (len >= 3) { // there are at least 3 characters to encode
+    uint32_t x = (src[0] << 16) | (src[1] << 8) | src[2];
+    *p++ = kBase64Chars[(x >> 18) & 0x3f];
+    *p++ = kBase64Chars[(x >> 12) & 0x3f];
+    *p++ = kBase64Chars[(x >> 6) & 0x3f];
+    *p++ = kBase64Chars[x & 0x3f];
+    len -= 3;
+    src += 3;
+  }
+  if (len == 1) { // there is only one character to encode
+    uint32_t x = src[0] << 16;
+    *p++ = kBase64Chars[(x >> 18) & 0x3f];
+    *p++ = kBase64Chars[(x >> 12) & 0x3f];
+    *p++ = '=';
+    *p++ = '=';
+  } else if (len == 2) { // there are two characters to encode
+    uint32_t x = (src[0] << 16) | (src[1] << 8);
+    *p++ = kBase64Chars[(x >> 18) & 0x3f];
+    *p++ = kBase64Chars[(x >> 12) & 0x3f];
+    *p++ = kBase64Chars[(x >> 6) & 0x3f];
+    *p++ = '=';
+  }
+  return std::string(out_buf, out_size);
 }
