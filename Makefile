a3716d3141ffc23dbd5c814cd31bc18fab558d37
Marek Rogalski <mafikpl@gmail.com>
2023-06-02 13:39:09 +0200

    End-to-end test

--- Makefile
+++ Makefile
@@ -12,10 +12,16 @@ clean :
 gatekeeper.tar.gz : gatekeeper gatekeeper.gif favicon.ico style.css gatekeeper.service
 	tar -czf $@ $^
 
+test : gatekeeper test_e2e.sh
+	sudo ./test_e2e.sh
+
 # Rules used by maf for development
 
 maf-run : gatekeeper
 	sudo ./gatekeeper enxe8802ee74415
+
+maf-valgrind : gatekeeper
+	sudo valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./gatekeeper enxe8802ee74415
 
 maf-deploy : gatekeeper.tar.gz
 	scp gatekeeper.tar.gz root@protectli:~/
---
+++ test_e2e.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+NS=ns0
+DEV=veth0
+
+DEV_A=${DEV}a
+DEV_B=${DEV}b
+
+if [ ! -e /run/netns/$NS ]; then
+  ip netns add $NS
+fi
+if [ ! -e /sys/class/net/$DEV_A ]; then
+  ip link add $DEV_A type veth peer name $DEV_B netns $NS
+fi
+ip addr flush dev $DEV_A
+ip netns exec $NS ip addr flush dev $DEV_B
+
+ip addr add 192.168.0.1/24 dev $DEV_A
+ip link set $DEV_A up
+ip netns exec $NS ip link set $DEV_B up
+
+./gatekeeper $DEV_A &
+GATEKEEPER_PID=$!
+
+cp -f test-dhclient-hook /etc/dhcp/dhclient-enter-hooks.d/
+ip netns exec $NS dhclient -1 -cf ./test-dhclient.conf $DEV_B
+
+CLIENT_IP=$(sudo ip netns exec ns0 hostname -I | xargs)
+HOSTNAME=$(hostname)
+DIG_HOSTNAME_LOCAL=$(ip netns exec $NS dig +short $HOSTNAME.local @192.168.0.1)
+CURL_1337=$(ip netns exec $NS curl -s http://192.168.0.1:1337)
+
+kill $GATEKEEPER_PID
+dhclient -x 2>/dev/null
+rm -f /etc/dhcp/dhclient-enter-hooks.d/test-dhclient-hook
+
+if [ "$CLIENT_IP" != "192.168.0.2" ]; then
+  echo "client IP is [$CLIENT_IP] but expected [192.168.0.2]"
+  exit 1
+fi
+
+if [ "$DIG_HOSTNAME_LOCAL" != "192.168.0.1" ]; then
+  echo "dig returned [$DIG_HOSTNAME_LOCAL] but expected [192.168.0.1]"
+  exit 1
+fi
+
+if [[ $CURL_1337 != *Gatekeeper* ]]; then
+  echo "http://192.168.0.1:1337 should contain [Gatekeeper]. Got [$CURL_1337]"
+  exit 1
+fi
