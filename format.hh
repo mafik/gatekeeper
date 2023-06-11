75c9e41edea5bdf9e184e5a56815573f18ec79fe
Marek Rogalski <mafikpl@gmail.com>
2023-06-11 14:45:14 +0200

    Function for dumping structs for debugging

--- format.hh
+++ format.hh
@@ -10,3 +10,20 @@ std::string f(const char *fmt, ...);
 std::string IndentString(std::string in, int spaces = 2);
 
 std::string Slugify(std::string in);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wformat-security"
+constexpr void constexpr_sprintf(std::string &out, const char *format,
+                                 auto... args) {
+  int n = snprintf(nullptr, 0, format, args...) + 1;
+  char buf[n];
+  snprintf(buf, n, format, args...);
+  out += buf;
+}
+#pragma clang diagnostic pop
+
+template <typename T> std::string dump_struct(const T &t) {
+  std::string s;
+  __builtin_dump_struct(&t, constexpr_sprintf, s);
+  return s;
+}
