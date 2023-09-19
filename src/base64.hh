abe95ed17d7535d7dc2b84aad06e95d73cc8ba5d
Marek Rogalski <mafikpl@gmail.com>
2023-09-19 15:21:44 +0200

    Fix base64.cc for older versions of the C++ standard

--- src/base64.hh
+++ src/base64.hh
@@ -6,9 +6,9 @@
 
 namespace maf {
 
-const char kBase64Chars[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                              "abcdefghijklmnopqrstuvwxyz"
-                              "0123456789+/";
+constexpr char kBase64Chars[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                  "abcdefghijklmnopqrstuvwxyz"
+                                  "0123456789+/";
 
 inline bool IsBase64(int c) { return (isalnum(c) || (c == '+') || (c == '/')); }
 
