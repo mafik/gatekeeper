b24bf488b2cc50895fccb2dbba43326dc65842e2
Marek Rogalski <mafikpl@gmail.com>
2023-10-11 10:35:16 +0200

    Prettier traffic graphs

--- static/script.js
+++ static/script.js
@@ -2,149 +2,171 @@ function AutorefreshChecked() {
   return document.getElementById("autorefresh").checked;
 }
 
+function ToPrecision(n, precision) {
+  if (n > (Math.pow(10, precision) - 1)) {
+    return Math.round(n);
+  } else {
+    return n.toPrecision(precision);
+  }
+}
+
+function FormatBytes(n) {
+  if (n >= 1024 * 1024) {
+    return ToPrecision(n / 1024 / 1024, 2) + ' MB';
+  } else if (n > 1024) {
+    return ToPrecision(n / 1024, 2) + ' kB';
+  } else {
+    return ToPrecision(n, 2) + ' B';
+  }
+}
+
+function FormatTime(ms) {
+  let ret = '';
+  function AppendUnit(milliseconds_per_unit, unit_name_singular, unit_name_plural) {
+    let whole_units = Math.floor(ms / milliseconds_per_unit);
+    if (whole_units) {
+      ms -= whole_units * milliseconds_per_unit;
+      ret += ' ' + whole_units + ' ' + (whole_units == 1 ? unit_name_singular : unit_name_plural);
+    }
+  }
+  AppendUnit(24 * 60 * 60 * 1000, 'day', 'days');
+  AppendUnit(60 * 60 * 1000, 'hour', 'hours');
+  AppendUnit(60 * 1000, 'minute', 'minutes');
+  AppendUnit(1000, 'second', 'seconds');
+  AppendUnit(1, 'ms', 'ms');
+  return ret.trim();
+}
+
+function RenderGraph(canvas) {
+  const N = 600;
+  const BarHeight = 100
+  const BarSpacing = 16;
+  const DownColor = 'rgb(52, 202, 56)';
+  const UpColor = 'rgb(255, 186, 0)';
+  const FontSize = 14;
+  const LineWidth = 1;
+
+  canvas.width = N;
+  canvas.height = 3 * BarHeight + 3 * BarSpacing;
+  let ctx = canvas.getContext('2d', { alpha: false });
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  let datapoints_json = canvas.innerHTML;
+  let datapoints = JSON.parse(datapoints_json);
+  let now = Date.now();
+
+  class Series {
+    constructor(symbol, color, milliseconds_length) {
+      this.symbol = symbol;
+      this.color = color;
+      this.data = new Uint32Array(N);
+      this.milliseconds_length = milliseconds_length;
+      this.milliseconds_per_pixel = milliseconds_length / N;
+    }
+    precomputeStats() {
+      this.max = this.data.reduce((a, b) => (b > a ? b : a), 0);
+      this.sum = this.data.reduce((a, b) => a + b, 0);
+      this.avg = this.sum / N;
+    }
+    static drawStatsLegend(offset_y, ctx) {
+      ctx.fillStyle = 'black';
+      ctx.textAlign = 'right';
+      ctx.fillText('Top', LineWidth + 70, offset_y + LineWidth);
+      ctx.fillText('Average', LineWidth + 130, offset_y + LineWidth);
+      ctx.fillText('Total', LineWidth + 190, offset_y + LineWidth);
+    }
+    drawStats(offset_y, ctx) {
+      ctx.fillStyle = this.color;
+      ctx.textAlign = 'left';
+      ctx.fillText(this.symbol, LineWidth, offset_y + LineWidth);
+      ctx.textAlign = 'right';
+      ctx.fillText(FormatBytes(this.max / this.milliseconds_per_pixel * 1000), LineWidth + 70, offset_y + LineWidth);
+      ctx.fillText(FormatBytes(this.avg / this.milliseconds_per_pixel * 1000), LineWidth + 130, offset_y + LineWidth);
+      ctx.fillText(FormatBytes(this.sum), LineWidth + 190, offset_y + LineWidth);
+    }
+  }
+
+  class Bar {
+    constructor(color, offset_y, milliseconds_length) {
+      this.color = color;
+      this.offset_y = offset_y;
+      this.milliseconds_length = milliseconds_length;
+      this.up = new Series('⬆', UpColor, milliseconds_length);
+      this.down = new Series('⬇', DownColor, milliseconds_length);
+      this.milliseconds_per_pixel = milliseconds_length / N;
+    }
+    addTraffic(time_up_down_arr) {
+      let i = Math.floor((now - time_up_down_arr[0]) / this.milliseconds_per_pixel);
+      if (i >= N) return;
+      this.up.data[i] += time_up_down_arr[1];
+      this.down.data[i] += time_up_down_arr[2];
+    }
+    precomputeStats() {
+      this.up.precomputeStats();
+      this.down.precomputeStats();
+    }
+    drawStats(ctx) {
+      Series.drawStatsLegend(this.offset_y, ctx);
+      this.down.drawStats(this.offset_y + FontSize, ctx);
+      this.up.drawStats(this.offset_y + FontSize * 2, ctx);
+    }
+    drawFrame(ctx) {
+      ctx.lineWidth = LineWidth;
+      ctx.font = FontSize + 'px Texturina';
+      ctx.strokeStyle = this.color;
+      ctx.strokeRect(LineWidth / 2, this.offset_y + LineWidth / 2, canvas.width - LineWidth, BarHeight - LineWidth);
+      ctx.fillStyle = 'black';
+      ctx.textBaseline = 'top';
+      ctx.textAlign = 'left';
+      ctx.fillText(FormatTime(this.milliseconds_length) + ' ago', 0, this.offset_y + BarHeight);
+      ctx.textAlign = 'center';
+      ctx.fillText('1 px = ' + FormatTime(this.milliseconds_per_pixel), canvas.width / 2, this.offset_y + BarHeight);
+      ctx.textAlign = 'right';
+      ctx.fillText('now', canvas.width - 1, this.offset_y + BarHeight);
+    }
+  }
+
+  let minute = new Bar('#c8c7b0', 0, 60 * 1000);
+  let hour = new Bar('#c8b0b5', BarHeight + BarSpacing, 60 * 60 * 1000);
+  let day = new Bar('#b6b0c8', BarHeight * 2 + BarSpacing * 2, 24 * 60 * 60 * 1000);
+  let bars = [minute, hour, day];
+
+  for (let i = 0; i < datapoints.length; i++) {
+    minute.addTraffic(datapoints[i]);
+    hour.addTraffic(datapoints[i]);
+    day.addTraffic(datapoints[i]);
+  }
+
+  bars.forEach((b) => b.precomputeStats());
+
+  bars.forEach((b) => b.drawFrame(ctx));
+
+  bars.forEach((b) => b.drawStats(ctx));
+
+  ctx.globalCompositeOperation = 'multiply';
+  ctx.fillStyle = DownColor;
+  for (let j = 0; j < bars.length; ++j) {
+    for (let i = 0; i < N; i++) {
+      if (bars[j].down.data[i] >= 0) {
+        let h = bars[j].down.data[i] / bars[j].down.max * BarHeight;
+        ctx.fillRect(canvas.width - 1 - i, bars[j].offset_y + BarHeight - h, 1, h);
+      }
+    }
+  }
+  ctx.fillStyle = UpColor;
+  for (let j = 0; j < bars.length; ++j) {
+    for (let i = 0; i < N; i++) {
+      if (bars[j].up.data[i] >= 0) {
+        let h = bars[j].up.data[i] / bars[j].up.max * BarHeight;
+        ctx.fillRect(canvas.width - 1 - i, bars[j].offset_y + BarHeight - h, 1, h);
+      }
+    }
+  }
+  ctx.globalCompositeOperation = 'source-over';
+}
+
 function RenderGraphs() {
-  document.querySelectorAll('canvas.traffic').forEach(function (canvas) {
-    let n = 600;
-    canvas.width = n;
-    const BarHeight = 100
-    const BarSpacing = 10;
-    canvas.height = 3 * BarHeight + 3 * BarSpacing;
-    let ctx = canvas.getContext('2d', { alpha: false });
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    let datapoints_json = canvas.innerHTML;
-    let datapoints = JSON.parse(datapoints_json);
-    let now = Date.now();
-
-    let minute_up = new Uint32Array(n);
-    let minute_down = new Uint32Array(n);
-    let hour_up = new Uint32Array(n);
-    let hour_down = new Uint32Array(n);
-    let day_up = new Uint32Array(n);
-    let day_down = new Uint32Array(n);
-
-    for (let i = 0; i < datapoints.length; i++) {
-      let [time, up, down] = datapoints[i];
-      let i_minute = Math.floor((now - time) / (100));
-      let i_hour = Math.floor(i_minute / 60);
-      let i_day = Math.floor(i_hour / 24);
-      if (i_minute < minute_down.length) {
-        minute_up[i_minute] += up;
-        minute_down[i_minute] += down;
-      }
-      if (i_hour < hour_down.length) {
-        hour_up[i_hour] += up;
-        hour_down[i_hour] += down;
-      }
-      if (i_day < day_down.length) {
-        day_up[i_day] += up;
-        day_down[i_day] += down;
-      }
-    }
-
-    let minute_up_max = 0, minute_down_max = 0, hour_up_max = 0, hour_down_max = 0, day_up_max = 0, day_down_max = 0;
-
-    for (let i = 0; i < n; i++) {
-      minute_up_max = Math.max(minute_up_max, minute_up[i]);
-      minute_down_max = Math.max(minute_down_max, minute_down[i]);
-      hour_up_max = Math.max(hour_up_max, hour_up[i]);
-      hour_down_max = Math.max(hour_down_max, hour_down[i]);
-      day_up_max = Math.max(day_up_max, day_up[i]);
-      day_down_max = Math.max(day_down_max, day_down[i]);
-    }
-
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = '#b6b0c8';
-    ctx.strokeRect(1, BarHeight * 2 + BarSpacing * 2 + 1, canvas.width - 2, BarHeight - 2);
-
-    ctx.translate(0, BarHeight + BarSpacing);
-
-    ctx.fillStyle = '#f8e0e5';
-    ctx.beginPath();
-    ctx.moveTo(canvas.width, BarHeight + BarSpacing);
-    ctx.lineTo(canvas.width - 10, BarHeight + BarSpacing);
-    ctx.bezierCurveTo(canvas.width - 10, BarHeight, 0, BarHeight + BarSpacing, 0, BarHeight);
-    ctx.lineTo(canvas.width, BarHeight);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.strokeStyle = '#c8b0b5';
-    ctx.strokeRect(1, 1, canvas.width - 2, BarHeight - 2);
-    ctx.strokeRect(canvas.width - 10 + 1, BarHeight + BarSpacing + 1, 10 - 2, BarHeight - 2);
-
-    ctx.translate(0, -BarHeight - BarSpacing);
-
-    ctx.fillStyle = '#f8f7e0';
-    ctx.beginPath();
-    ctx.moveTo(canvas.width, BarHeight + BarSpacing);
-    ctx.lineTo(canvas.width - 10, BarHeight + BarSpacing);
-    ctx.bezierCurveTo(canvas.width - 10, BarHeight, 0, BarHeight + BarSpacing, 0, BarHeight);
-    ctx.lineTo(canvas.width, BarHeight);
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.strokeStyle = '#c8c7b0';
-    ctx.lineWidth = 2;
-    ctx.strokeRect(1, 1, canvas.width - 2, BarHeight - 2);
-    ctx.strokeRect(canvas.width - 10 + 1, BarHeight + BarSpacing + 1, 10 - 2, BarHeight - 2);
-
-    ctx.font = '10px Texturina';
-    ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.textBaseline = 'top';
-    ctx.fillText('1 minute ago', 0, BarHeight);
-    ctx.fillText('1 hour ago', 0, 2 * BarHeight + BarSpacing);
-    ctx.fillText('1 day ago', 0, 3 * BarHeight + 2 * BarSpacing);
-    ctx.textAlign = 'right';
-    ctx.fillText('now', canvas.width - 1, BarHeight);
-    ctx.fillText('now', canvas.width - 1, 2 * BarHeight + BarSpacing);
-    ctx.fillText('now', canvas.width - 1, 3 * BarHeight + 2 * BarSpacing);
-
-    const DownColor = '#31962d';
-    const UpColor = '#91762c';
-    ctx.fillStyle = DownColor;
-    ctx.textAlign = 'left';
-    ctx.fillText('max ⬇ = ' + Math.round(minute_down_max * 10) + ' B/s', 2, 2);
-    ctx.fillText('max ⬇ = ' + Math.round(hour_down_max * 10 / 60) + ' B/s', 2, 2 + BarHeight + BarSpacing);
-    ctx.fillText('max ⬇ = ' + Math.round(day_down_max * 10 / 60 / 24) + ' B/s', 2, 2 + BarHeight * 2 + BarSpacing * 2);
-    ctx.fillStyle = UpColor;
-    ctx.fillText('max ⬆ = ' + Math.round(minute_up_max * 10) + ' B/s', 2, 12);
-    ctx.fillText('max ⬆ = ' + Math.round(hour_up_max * 10 / 60) + ' B/s', 2, 12 + BarHeight + BarSpacing);
-    ctx.fillText('max ⬆ = ' + Math.round(day_up_max * 10 / 60 / 24) + ' B/s', 2, 12 + BarHeight * 2 + BarSpacing * 2);
-
-
-    ctx.globalCompositeOperation = 'multiply';
-    ctx.fillStyle = DownColor;
-    for (let i = 0; i < n; i++) {
-      if (minute_down[i] >= 0) {
-        let h = minute_down[i] / minute_down_max * BarHeight;
-        ctx.fillRect(canvas.width - 1 - i, BarHeight - h, 1, h);
-      }
-      if (hour_down[i] >= 0) {
-        let h = hour_down[i] / hour_down_max * BarHeight;
-        ctx.fillRect(canvas.width - 1 - i, 2 * BarHeight + BarSpacing - h, 1, h);
-      }
-      if (day_down[i] >= 0) {
-        let h = day_down[i] / day_down_max * BarHeight;
-        ctx.fillRect(canvas.width - 1 - i, 3 * BarHeight + 2 * BarSpacing - h, 1, h);
-      }
-    }
-    ctx.fillStyle = UpColor;
-    for (let i = 0; i < n; i++) {
-      if (minute_up[i] >= 0) {
-        let h = minute_up[i] / minute_up_max * BarHeight;
-        ctx.fillRect(canvas.width - 1 - i, BarHeight - h, 1, h);
-      }
-      if (hour_up[i] >= 0) {
-        let h = hour_up[i] / hour_up_max * BarHeight;
-        ctx.fillRect(canvas.width - 1 - i, 2 * BarHeight + BarSpacing - h, 1, h);
-      }
-      if (day_up[i] >= 0) {
-        let h = day_up[i] / day_up_max * BarHeight;
-        ctx.fillRect(canvas.width - 1 - i, 3 * BarHeight + 2 * BarSpacing - h, 1, h);
-      }
-    }
-    ctx.globalCompositeOperation = 'source-over';
-  });
+  document.querySelectorAll('canvas.traffic').forEach(RenderGraph);
 }
 
 document.addEventListener("DOMContentLoaded", RenderGraphs);
