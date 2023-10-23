2e801e01ebf16603fbc430bafcddd4108724cda4
Marek Rogalski <mafikpl@gmail.com>
2023-10-23 22:23:47 +0200

    Mixin class for expirable objects

---
+++ src/expirable.cc
@@ -0,0 +1,74 @@
+#include "expirable.hh"
+
+#include <cmath>
+#include <set>
+
+using namespace std;
+
+namespace maf {
+
+struct OrderByExpiration {
+  using is_transparent = true_type;
+  bool operator()(const Expirable *a, const Expirable *b) const {
+    return a->expiration < b->expiration;
+  }
+  bool operator()(const Expirable *a,
+                  chrono::steady_clock::time_point b) const {
+    return a->expiration < b;
+  }
+  bool operator()(chrono::steady_clock::time_point a,
+                  const Expirable *b) const {
+    return a < b->expiration;
+  }
+};
+
+static multiset<Expirable *, OrderByExpiration> expiration_queue;
+
+Expirable::Expirable() : expiration(nullopt) {}
+
+Expirable::Expirable(chrono::steady_clock::time_point expiration)
+    : expiration(expiration) {
+  AddToExpirationQueue();
+}
+
+Expirable::Expirable(chrono::steady_clock::duration ttl)
+    : Expirable(chrono::steady_clock::now() + ttl) {}
+
+Expirable::~Expirable() { RemoveFromExpirationQueue(); }
+
+void Expirable::AddToExpirationQueue() {
+  if (expiration == nullopt) {
+    return;
+  }
+  expiration_queue.insert(this);
+}
+
+void Expirable::RemoveFromExpirationQueue() const {
+  if (expiration == nullopt) {
+    return;
+  }
+  auto [begin, end] = expiration_queue.equal_range(*expiration);
+  for (auto it = begin; it != end; ++it) {
+    if (*it == this) {
+      expiration_queue.erase(it);
+      break;
+    }
+  }
+}
+
+void Expirable::UpdateExpiration(
+    chrono::steady_clock::time_point new_expiration) {
+  RemoveFromExpirationQueue();
+  expiration = new_expiration;
+  expiration_queue.insert(this);
+}
+
+void Expirable::Expire() {
+  auto now = chrono::steady_clock::now();
+  while (!expiration_queue.empty() &&
+         (*expiration_queue.begin())->expiration < now) {
+    delete *expiration_queue.begin();
+  }
+}
+
+} // namespace maf
---
+++ src/expirable.hh
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <chrono>
+
+#include "optional.hh"
+
+namespace maf {
+
+// Mixin class that can be used to automatically delete objects after a certain
+// expiration time.
+//
+// Expirable objects can be refreshed by calling `UpdateExpiration`.
+//
+// Expirable objects are deleted by the `Expire` function, which should be
+// called periodically.
+struct Expirable {
+  // Don't modify directly. Use `UpdateExpiration` instead.
+  Optional<std::chrono::steady_clock::time_point> expiration;
+
+  // Initialize without inserting into expiration queue.
+  Expirable();
+
+  // Initialize and insert into expiration queue with the given expiration.
+  Expirable(std::chrono::steady_clock::time_point expiration);
+
+  // Initialize and insert into expiration queue with the given TTL.
+  Expirable(std::chrono::steady_clock::duration ttl);
+
+  // Destructor automatically removes `this` from the expiration queue.
+  virtual ~Expirable();
+
+  // O(log n)
+  void UpdateExpiration(std::chrono::steady_clock::time_point new_expiration);
+
+  // O(1)
+  static void Expire();
+
+private:
+  void AddToExpirationQueue();
+  void RemoveFromExpirationQueue() const;
+};
+
+} // namespace maf
