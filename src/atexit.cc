ec15a9f537e5ae30fc7817c7dd75974f07ab9e4f
Marek Rogalski <mafikpl@gmail.com>
2023-10-25 17:07:12 +0200

    Fix AtExit initialization

--- src/atexit.cc
+++ src/atexit.cc
@@ -6,13 +6,19 @@ namespace maf {
 
 Vec<Fn<void()>> at_exit_functions;
 
-__attribute__((constructor)) void InitAtExit() { atexit(ExitCleanup); }
-
-void AtExit(Fn<void()> f) { at_exit_functions.push_back(std::move(f)); }
+void AtExit(Fn<void()> f) {
+  static bool initialized = false;
+  if (!initialized) {
+    atexit(ExitCleanup);
+    initialized = true;
+  }
+  at_exit_functions.push_back(std::move(f));
+}
 
 void ExitCleanup() {
-  for (auto &f : at_exit_functions) {
-    f();
+  while (!at_exit_functions.empty()) {
+    at_exit_functions.back()();
+    at_exit_functions.pop_back();
   }
 }
 
