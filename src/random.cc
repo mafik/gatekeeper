e0477f2910f506bf3511a77ef14d8702a79e075a
Marek Rogalski <mafikpl@gmail.com>
2023-11-06 10:19:31 +0100

    Function for secure random bytes

--- src/random.cc
+++ src/random.cc
@@ -1,4 +1,15 @@
 #include "random.hh"
+#include "int.hh"
+
+#include <sys/random.h>
 
 std::random_device rand_dev;
 std::mt19937 generator(rand_dev());
+
+namespace maf {
+
+void RandomBytesSecure(Span<> out) {
+  SSize n = getrandom(out.data(), out.size(), 0);
+}
+
+} // namespace maf
--- src/random.hh
+++ src/random.hh
@@ -2,6 +2,8 @@
 
 #include <random>
 
+#include "span.hh"
+
 extern std::mt19937 generator;
 
 template <typename T> T random() {
@@ -9,3 +11,12 @@ template <typename T> T random() {
                                          std::numeric_limits<T>::max());
   return distr(generator);
 }
+
+namespace maf {
+
+// This function may block if there is not enough entropy available.
+//
+// See `man 2 getrandom` for more information.
+void RandomBytesSecure(Span<> out);
+
+} // namespace maf
