556705fb97669afdf56423b0b25ce62be6290f64
Marek Rogalski <mafikpl@gmail.com>
2026-04-05 16:42:58 +0200

    Make the LAN ARP entries permanent

--- src/arp.cc
+++ src/arp.cc
@@ -1,8 +1,9 @@
 #include "arp.hh"
-#include "status.hh"
 
 #include <net/if_arp.h>
 #include <sys/ioctl.h>
+
+#include "status.hh"
 
 namespace automat::arp {
 
@@ -17,16 +18,15 @@ struct IOCtlRequest {
 static_assert(sizeof(IOCtlRequest) == sizeof(arpreq),
               "IOCtlRequest doesn't match `struct arpreq` from <net/if_arp.h>");
 
-void Set(const std::string &interface, IP ip, MAC mac, int af_inet_fd,
-         Status &status) {
+void Set(const std::string& interface, IP ip, MAC mac, int af_inet_fd, Status& status) {
   IOCtlRequest r{
-      .protocol_address = {.sin_family = AF_INET,
-                           .sin_addr = {.s_addr = ip.addr}},
+      .protocol_address = {.sin_family = AF_INET, .sin_addr = {.s_addr = ip.addr}},
       .hardware_address = {.sa_family = AF_UNSPEC,
-                           .sa_data = {(char)mac[0], (char)mac[1], (char)mac[2],
-                                       (char)mac[3], (char)mac[4],
-                                       (char)mac[5]}},
-      .flags = ATF_COM,
+                           .sa_data = {(char)mac[0], (char)mac[1], (char)mac[2], (char)mac[3],
+                                       (char)mac[4], (char)mac[5]}},
+      .flags = ATF_COM | ATF_PERM,  // PERM helps with reponsiveness power-saving IoT devices that
+                                    // don't respond to unicast ARP requests, and have to be woken
+                                    // up by a broadcast ARP (delivered during DTIM).
   };
   strncpy(r.device, interface.c_str(), sizeof(r.device));
   if (ioctl(af_inet_fd, SIOCSARP, &r) < 0) {
@@ -34,4 +34,4 @@ void Set(const std::string &interface, IP ip, MAC mac, int af_inet_fd,
   }
 }
 
-} // namespace automat::arp
+}  // namespace automat::arp
